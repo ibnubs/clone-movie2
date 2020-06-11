458fc9bb22777c69b77ff70579760d1c131cc4c5
ibnubs <ibnubs@gmail.com>
2020-06-11 15:03:32 +0700

    [ibnu]-clean trace update

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "../assets/style/style.css";
+
+
+
+export default function Navbar() {
+  return (
+    <Fragment>
+      <h1>test</h1>
+    </Fragment>
+  );
+}
---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+import Navbar from '../components/Navbar'
+
+
+export default function Homepage() {
+    return (
+        <Fragment>
+            <Navbar />
+        </Fragment>
+    )
+}
