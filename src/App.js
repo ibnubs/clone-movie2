fb05773661395a8ec043ad3eb7f38318a38ae844
feryrmdhnc <feryramadhan555@gmail.com>
2020-06-11 17:55:57 +0800

    done-login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import Store from "./store";
+import LoginForm from "./components/Login";
 import Footers from "./components/Footers";
 
 function App() {
   return (
     <Provider store={Store}>
       <div>
-        <h1>Hello world</h1>
+        <LoginForm />
         <Footers />
       </div>
     </Provider>
