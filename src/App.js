0f815ff78b13cd752e45bd7b612186be81aae2c9
ibnubs <ibnubs@gmail.com>
2020-06-12 17:52:54 +0700

    [ibnu]-branch untuk join file sblm kedev, join footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import Routes from './routers/Routes';
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+
 
 
 
 function App() {
   return (
-    <div className="">
-      <Routes />
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="">
+          <Routes />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
