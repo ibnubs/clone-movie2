55aa4e83eaf31452a6d63f44f211cd2eae2228d5
ibnubs <ibnubs@gmail.com>
2020-06-17 09:08:00 +0700

    delete getmovie from reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './Auth';
 import profiles from './profile';
-import { listMovies } from './movies';
 import review from './review';
 
 const rootReducers = combineReducers({
     auth: authReducer,
     profile: profiles,
-    get_movies: listMovies,
     add_review: review
 })
 
